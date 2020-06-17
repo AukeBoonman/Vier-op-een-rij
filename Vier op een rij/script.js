16d274c8ab8eca8706fae03625b33180b9e0067a
AukeBoonman <auke.boonman@gmail.com>
2020-06-17 21:55:25 +0200

    kolom kleurt en losse cellen worden actief-geen val

--- Vier op een rij/script.js
+++ Vier op een rij/script.js
@@ -1,38 +1,21 @@
-//click on column (tr)
-
-var column1 = document.getElementById("col1")
-var column2 = document.getElementById("col2")
-var column3 = document.getElementById("col3")
-var column4 = document.getElementById("col4")
-var column5 = document.getElementById("col5")
-var column6 = document.getElementById("col6")
-var column7 = document.getElementById("col7")
-
-column1.onclick = function(){
-    document.getElementById("td16").style.background = "#e67e22";
-}
-
-column2.onclick = function(){
-    document.getElementById("td26").style.background = "#e67e22";
-}
-column3.onclick = function(){
-    document.getElementById("td36").style.background = "#e67e22";
-}
-column4.onclick = function(){
-    document.getElementById("td46").style.background = "#e67e22";
-}
-column5.onclick = function(){
-    document.getElementById("td56").style.background = "#e67e22";
-}
-column6.onclick = function(){
-    document.getElementById("td66").style.background = "#e67e22";
-}
-column7.onclick = function(){
-    document.getElementById("td76").style.background = "#e67e22";
-}
+//CURRENTLY - THIS ALLOWS ME TO 'ACTIVATE' CELLS, BUT THEY DON'T 'DROP' TO THE BOTTOM.
 
 
 
+var col1 = document.getElementById("tr1");
+var col2 = document.getElementById("tr1");
 
-// change color of lowest td.
+var cells1 = col1.getElementsByClassName("cell");
 
+
+for (var i = 0; i < cells1.length; i++) {
+    cells1[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+
+        /*if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }*/
+
+        this.className += " active";
+    });
+};
