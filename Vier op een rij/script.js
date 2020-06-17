1a8b20d2f4fa895414348062219931d6a94c5723
AukeBoonman <auke.boonman@gmail.com>
2020-06-17 11:00:27 +0200

    tabel kolom-hover onderste-rij-kleur

---
+++ Vier op een rij/script.js
@@ -0,0 +1,38 @@
+//click on column (tr)
+
+var column1 = document.getElementById("col1")
+var column2 = document.getElementById("col2")
+var column3 = document.getElementById("col3")
+var column4 = document.getElementById("col4")
+var column5 = document.getElementById("col5")
+var column6 = document.getElementById("col6")
+var column7 = document.getElementById("col7")
+
+column1.onclick = function(){
+    document.getElementById("td16").style.background = "#e67e22";
+}
+
+column2.onclick = function(){
+    document.getElementById("td26").style.background = "#e67e22";
+}
+column3.onclick = function(){
+    document.getElementById("td36").style.background = "#e67e22";
+}
+column4.onclick = function(){
+    document.getElementById("td46").style.background = "#e67e22";
+}
+column5.onclick = function(){
+    document.getElementById("td56").style.background = "#e67e22";
+}
+column6.onclick = function(){
+    document.getElementById("td66").style.background = "#e67e22";
+}
+column7.onclick = function(){
+    document.getElementById("td76").style.background = "#e67e22";
+}
+
+
+
+
+// change color of lowest td.
+
